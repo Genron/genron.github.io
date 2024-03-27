12b3234e2d4b39fe2eee2bd2b3337d954bc355ef
Marco Sanfratello <marco.sanfratello@karakun.com>
2024-03-27 08:09:08 +0100

    rudimentary import of json

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as data from './data/data_de.json'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <code>
+        <pre>{JSON.stringify(data, null, 4)}</pre>
+      </code>
     </div>
   );
 }
